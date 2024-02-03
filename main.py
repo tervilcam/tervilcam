2b906375cf4e3f0cb452da5e3e10d0337386fea4
tervilcam <158653079+tervilcam@users.noreply.github.com>
2024-02-02 21:43:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,6 @@ import streamlit as st
 
 import pandas as pd 
 
-import numpy as np 
-
-st.title('Titulo') 
-n = st.slider("n", 5,100, step=1)
-
-chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
-
-st.line_chart(chart_data)
-
-PETER MONTALVO GARCIA8:49 PM
-import streamlit as st 
-
-import pandas as pd 
-
 import numpy as np
 
 
